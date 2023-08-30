fcc649ddd5949ed67cdd730e9119c0b49b16b84f
YYHEggEgg <53960525+YYHEggEgg@users.noreply.github.com>
2023-08-30 12:52:57 +0800

    Migrated to win7 release and pack runtime

--- create-release.bat
+++ create-release.bat
@@ -1,7 +1,11 @@
-mkdir .\Release-windows10-x64
-mkdir .\Release-windows10-x64\Updater
-xcopy .\HappyGenyuanImsactUpdate\bin\Release\net6.0-windows10.0.17763.0 .\Release-windows10-x64\Updater\ /s /e /y
-mkdir ".\Release-windows10-x64\Patch Creator"
-xcopy .\HDiffPatchCreator\bin\Release\net6.0-windows10.0.17763.0 ".\Release-windows10-x64\Patch Creator\" /s /e /y
+mkdir .\Release-windows7-x64
+dotnet publish --configuration Release .\HappyGenyuanImsactUpdate --output .\Release-windows7-x64\Updater
+copy ".\HappyGenyuanImsactUpdate\bin\Release\net6.0-windows7.0\7z.exe" ".\Release-windows7-x64\Updater\7z.exe" /y
+copy ".\HappyGenyuanImsactUpdate\bin\Release\net6.0-windows7.0\hpatchz.exe" ".\Release-windows7-x64\Updater\hpatchz.exe" /y
+
+dotnet publish --configuration Release .\HDiffPatchCreator --output ".\Release-windows7-x64\Patch Creator"
+copy ".\HDiffPatchCreator\bin\Release\net6.0-windows7.0\7z.exe" ".\Release-windows7-x64\Patch Creator\7z.exe" /y
+copy ".\HDiffPatchCreator\bin\Release\net6.0-windows7.0\hdiffz.exe" ".\Release-windows7-x64\Patch Creator\hdiffz.exe" /y
+copy ".\HDiffPatchCreator\bin\Release\net6.0-windows7.0\hpatchz.exe" ".\Release-windows7-x64\Patch Creator\hpatchz.exe" /y
 
 pause
